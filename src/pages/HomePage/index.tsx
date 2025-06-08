61445cb3563383a725b6393b172aa3a26f5099da
PeterTakahashi <twitter333555@gmail.com>
2025-06-08 17:58:15 +0800

    fix home

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,5 @@
 import type { FC } from "react";
 import { useVerifiedUser } from "@/features/hooks/swr/fetcher/user/useVerifiedUser";
-import { toUsdFloat } from "@/lib/toUsdFloat";
 
 export const HomePage: FC = () => {
   const { user } = useVerifiedUser();
@@ -10,12 +9,7 @@ export const HomePage: FC = () => {
   return (
     <div>
       <div className="flex flex-col items-center justify-center px-4 py-4">
-        <h1 className="text-4xl font-bold mb-4">Welcome, {user.email}!</h1>
-        <p className="text-lg text-gray-700">You have verified your account.</p>
-        <p className="text-lg text-gray-700 mt-2">
-          Your wallet balance is:{" "}
-          <strong>${toUsdFloat(user.wallet?.balance)}</strong>
-        </p>
+        <h1 className="text-4xl font-bold mb-4">Welcome</h1>
       </div>
     </div>
   );
