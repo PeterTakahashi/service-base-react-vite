c517decb8ab1c09327db09c8e2634cba0d8405f0
PeterTakahashi <twitter333555@gmail.com>
2025-06-20 23:46:11 +0800

    add

--- src/components/atoms/ToggleGroup/index.tsx
+++ src/components/atoms/ToggleGroup/index.tsx
@@ -3,7 +3,7 @@ import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group";
 import { type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils/cn";
-import { toggleVariants } from "@/components/atoms/Toggle";
+import { toggleVariants } from "@/components/atoms/Toggle/toggleVariants";
 
 const ToggleGroupContext = React.createContext<
   VariantProps<typeof toggleVariants>
