8656f61dc27675841d3aa8894153d1955cbf9180
PeterTakahashi <twitter333555@gmail.com>
2025-06-26 22:45:24 +0800

    add schema

--- src/types/api/user/user.ts
+++ src/types/api/user/user.ts
@@ -2,7 +2,7 @@ import type { components } from "@/types/api/base";
 
 export type UserRead = components["schemas"]["UserRead"];
 
-export type UserWithUserWalletRead =
-  components["schemas"]["UserWithUserWalletRead"];
+export type UserWithRelationRead =
+  components["schemas"]["UserWithRelationRead"];
 
 export type UserUpdate = components["schemas"]["UserUpdate"];
